d042acf5b2c98b52f8c167688799794632211b85
Yun-Hsuan <hunterlj3201@gmail.com>
2017-10-11 20:15:20 +0800

    Tutorial files

---
+++ WebServer.js
@@ -0,0 +1,21 @@
+const http = require('http')
+
+// Build a HttpServer
+// req (Request) ask from clients.
+// res (Response) return states from server.
+var server = http.createServer(function(req, res){
+
+    var resHeader = {
+        'Content-Type':'text/html; charset=utf-8 ',
+        'Accept-Language':'utf-8',
+        'Accept-Charset':'utf-8',
+    }
+    res.writeHead(200, resHeader)
+    res.write('<h1>Hello</h1>', 'utf8');
+    res.write('<p>這是由node.js建立的WebServer</p>', 'utf8');
+    res.end();
+});
+
+// Open port 1234 and run the server.
+server.listen(1234)
+console.log("Server running at prot http://127.0.0.1:1234");
